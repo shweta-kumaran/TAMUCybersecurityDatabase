3aaf03216f1cea455470218ce3f7917dd18fd8bb
shweta-kumaran <89658334+shweta-kumaran@users.noreply.github.com>
2023-11-21 20:57:46 -0600

    Starter files for the project

---
+++ tamucc/tamuccdb.sql
@@ -0,0 +1,6 @@
+-- phpMyAdmin commands to create the database locally
+-- open up localhost/phpmyadmin
+-- create a new database called tamuccdb
+-- open up the database and go to the sql tab
+-- put these commands below to create the database locally
+
