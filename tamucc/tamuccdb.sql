0212bf3e89765fd59a052734581a89b319f6eb0d
shweta-kumaran <89658334+shweta-kumaran@users.noreply.github.com>
2023-11-26 13:55:31 -0600

    Update tamuccdb.sql

--- tamucc/tamuccdb.sql
+++ tamucc/tamuccdb.sql
@@ -10,12 +10,11 @@ CREATE TABLE users (
     First_Name VARCHAR(255) NOT NULL,
     M_Initial VARCHAR(1),
     Last_Name VARCHAR(255) NOT NULL,
-    Username VARCHAR(30) NOT NULL,
+    Username VARCHAR(150) NOT NULL,
     Passwords VARCHAR(255) NOT NULL,
-    User_Type VARCHAR(10) NOT NULL,
-    -- find a way to creat user_type a determining key for college student or admin view
-    Email VARCHAR(100),
-    Discord_Name VARCHAR(100),
+    User_Type VARCHAR(50) NOT NULL,
+    Email VARCHAR(150),
+    Discord_Name VARCHAR(150),
     PRIMARY KEY (UIN)
 );
 
