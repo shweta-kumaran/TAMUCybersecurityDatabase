e719a29229b4006b7ccbcd5865df78d68cfa37ff
julianaLeano <julexd125@gmail.com>
2023-12-03 15:22:54 -0600

    comment statment

--- tamucc/index.php
+++ tamucc/index.php
@@ -9,7 +9,7 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    echo "Connected successfully";
+    // echo "Connected successfully";
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $username = $_POST['username'];
