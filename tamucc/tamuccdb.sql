0155d703265ec138b228d768637c0e500641a812
shweta-kumaran <shweta.kumaran@gmail.com>
2023-12-05 20:35:23 -0600

    Updated sql

--- tamucc/tamuccdb.sql
+++ tamucc/tamuccdb.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 29, 2023 at 03:38 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: localhost
+-- Generation Time: Dec 06, 2023 at 03:28 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,21 @@ SET time_zone = "+00:00";
 --
 -- Database: `tamuccdb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `application`
+--
+
+CREATE TABLE `application` (
+  `App_NUM` int(11) NOT NULL,
+  `Program_Num` int(11) NOT NULL,
+  `UIN` int(11) NOT NULL,
+  `Uncom_Cert` varchar(255) DEFAULT NULL,
+  `Com_Cert` varchar(255) DEFAULT NULL,
+  `Purpose_Statement` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -104,6 +119,83 @@ CREATE TABLE `collegestudents` (
   `Student_Type` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `collegestudents`
+--
+
+INSERT INTO `collegestudents` (`UIN`, `Gender`, `HispanicLatino`, `Race`, `USCitizen`, `First_Generation`, `DoB`, `GPA`, `Major`, `Minor1`, `Minor2`, `Expected_Graduation`, `School`, `Current_Classification`, `Phone`, `Student_Type`) VALUES
+(2, 'Male', 0x30, 'Caucasian', 0x31, 0x30, '1990-01-15', 3.5, 'Computer Science', 'Mathematics', 'Psychology', 2023, 'School of Science and Engineering', 'Senior', 1234567890, 'Undergraduate');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `documentation`
+--
+
+CREATE TABLE `documentation` (
+  `Doc_Num` int(11) NOT NULL,
+  `App_Num` int(11) NOT NULL,
+  `Student_Num` int(11) NOT NULL,
+  `Link` varchar(255) NOT NULL,
+  `Doc_Type` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event`
+--
+
+CREATE TABLE `event` (
+  `Event_ID` int(10) NOT NULL,
+  `UIN` int(11) NOT NULL,
+  `Program_Num` int(10) NOT NULL,
+  `Start_Date` date NOT NULL,
+  `Time` time NOT NULL,
+  `Location` varchar(255) NOT NULL,
+  `End_Date` date NOT NULL,
+  `Event_Type` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_tracking`
+--
+
+CREATE TABLE `event_tracking` (
+  `ET_NUM` int(11) NOT NULL,
+  `Event_ID` int(11) NOT NULL,
+  `UIN` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `internship`
+--
+
+CREATE TABLE `internship` (
+  `Intern_ID` int(11) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Description` varchar(255) NOT NULL,
+  `is_Gov` binary(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `intern_app`
+--
+
+CREATE TABLE `intern_app` (
+  `IA_Num` int(10) NOT NULL,
+  `UIN` int(11) NOT NULL,
+  `Intern_ID` int(10) NOT NULL,
+  `Status` varchar(255) NOT NULL,
+  `Year` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- --------------------------------------------------------
 
 --
@@ -114,6 +206,18 @@ CREATE TABLE `programs` (
   `Program_Num` int(10) NOT NULL,
   `Prog_Name` varchar(255) NOT NULL,
   `Prog_Des` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `track`
+--
+
+CREATE TABLE `track` (
+  `Tracking_Num` int(11) NOT NULL,
+  `Program_Num` int(11) NOT NULL,
+  `Student_Num` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -131,12 +235,28 @@ CREATE TABLE `users` (
   `Passwords` varchar(255) NOT NULL,
   `User_Type` varchar(50) NOT NULL,
   `Email` varchar(150) NOT NULL,
-  `Discord_Name` varchar(150) NOT NULL,
+  `Discord_Name` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`UIN`, `First_Name`, `M_Initial`, `Last_Name`, `Username`, `Passwords`, `User_Type`, `Email`, `Discord_Name`) VALUES
+(1, 'Admin', NULL, 'User', 'admin', 'admin', 'admin', 'admin@example.com', 'admin#1234'),
+(2, 'John', 'M', 'Doe', 'john.doe', 'password', 'student', 'john.doe@example.com', 'john.doe#5678');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `application`
+--
+ALTER TABLE `application`
+  ADD PRIMARY KEY (`App_NUM`),
+  ADD KEY `Program_Num` (`Program_Num`),
+  ADD KEY `UIN` (`UIN`);
 
 --
 -- Indexes for table `certification`
@@ -174,10 +294,55 @@ ALTER TABLE `collegestudents`
   ADD PRIMARY KEY (`UIN`);
 
 --
+-- Indexes for table `documentation`
+--
+ALTER TABLE `documentation`
+  ADD PRIMARY KEY (`Doc_Num`),
+  ADD KEY `App_Num` (`App_Num`);
+
+--
+-- Indexes for table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`Event_ID`),
+  ADD KEY `UIN` (`UIN`),
+  ADD KEY `Program_Num` (`Program_Num`);
+
+--
+-- Indexes for table `event_tracking`
+--
+ALTER TABLE `event_tracking`
+  ADD PRIMARY KEY (`ET_NUM`),
+  ADD KEY `Event_ID` (`Event_ID`),
+  ADD KEY `UIN` (`UIN`);
+
+--
+-- Indexes for table `internship`
+--
+ALTER TABLE `internship`
+  ADD PRIMARY KEY (`Intern_ID`);
+
+--
+-- Indexes for table `intern_app`
+--
+ALTER TABLE `intern_app`
+  ADD PRIMARY KEY (`IA_Num`),
+  ADD KEY `UIN` (`UIN`),
+  ADD KEY `Intern_ID` (`Intern_ID`);
+
+--
 -- Indexes for table `programs`
 --
 ALTER TABLE `programs`
   ADD PRIMARY KEY (`Program_Num`);
+
+--
+-- Indexes for table `track`
+--
+ALTER TABLE `track`
+  ADD PRIMARY KEY (`Tracking_Num`),
+  ADD KEY `Program_Num` (`Program_Num`),
+  ADD KEY `Student_Num` (`Student_Num`);
 
 --
 -- Indexes for table `users`
@@ -186,8 +351,91 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`UIN`);
 
 --
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `application`
+--
+ALTER TABLE `application`
+  MODIFY `App_NUM` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `certification`
+--
+ALTER TABLE `certification`
+  MODIFY `Cert_ID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `cert_enrollment`
+--
+ALTER TABLE `cert_enrollment`
+  MODIFY `CertE_Num` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `classes`
+--
+ALTER TABLE `classes`
+  MODIFY `Class_ID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `class_enrollment`
+--
+ALTER TABLE `class_enrollment`
+  MODIFY `CE_Num` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `documentation`
+--
+ALTER TABLE `documentation`
+  MODIFY `Doc_Num` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `event`
+--
+ALTER TABLE `event`
+  MODIFY `Event_ID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `event_tracking`
+--
+ALTER TABLE `event_tracking`
+  MODIFY `ET_NUM` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `internship`
+--
+ALTER TABLE `internship`
+  MODIFY `Intern_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `intern_app`
+--
+ALTER TABLE `intern_app`
+  MODIFY `IA_Num` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `programs`
+--
+ALTER TABLE `programs`
+  MODIFY `Program_Num` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `track`
+--
+ALTER TABLE `track`
+  MODIFY `Tracking_Num` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `application`
+--
+ALTER TABLE `application`
+  ADD CONSTRAINT `application_ibfk_1` FOREIGN KEY (`Program_Num`) REFERENCES `programs` (`Program_Num`),
+  ADD CONSTRAINT `application_ibfk_2` FOREIGN KEY (`UIN`) REFERENCES `collegestudents` (`UIN`);
 
 --
 -- Constraints for table `cert_enrollment`
@@ -209,88 +457,42 @@ ALTER TABLE `class_enrollment`
 --
 ALTER TABLE `collegestudents`
   ADD CONSTRAINT `collegestudents_ibfk_1` FOREIGN KEY (`UIN`) REFERENCES `users` (`UIN`);
+
+--
+-- Constraints for table `documentation`
+--
+ALTER TABLE `documentation`
+  ADD CONSTRAINT `documentation_ibfk_1` FOREIGN KEY (`App_Num`) REFERENCES `application` (`App_NUM`);
+
+--
+-- Constraints for table `event`
+--
+ALTER TABLE `event`
+  ADD CONSTRAINT `event_ibfk_1` FOREIGN KEY (`UIN`) REFERENCES `users` (`UIN`),
+  ADD CONSTRAINT `event_ibfk_2` FOREIGN KEY (`Program_Num`) REFERENCES `programs` (`Program_Num`);
+
+--
+-- Constraints for table `event_tracking`
+--
+ALTER TABLE `event_tracking`
+  ADD CONSTRAINT `event_tracking_ibfk_1` FOREIGN KEY (`Event_ID`) REFERENCES `event` (`Event_ID`),
+  ADD CONSTRAINT `event_tracking_ibfk_2` FOREIGN KEY (`UIN`) REFERENCES `users` (`UIN`);
+
+--
+-- Constraints for table `intern_app`
+--
+ALTER TABLE `intern_app`
+  ADD CONSTRAINT `intern_app_ibfk_1` FOREIGN KEY (`UIN`) REFERENCES `collegestudents` (`UIN`),
+  ADD CONSTRAINT `intern_app_ibfk_2` FOREIGN KEY (`Intern_ID`) REFERENCES `internship` (`Intern_ID`);
+
+--
+-- Constraints for table `track`
+--
+ALTER TABLE `track`
+  ADD CONSTRAINT `track_ibfk_1` FOREIGN KEY (`Program_Num`) REFERENCES `programs` (`Program_Num`),
+  ADD CONSTRAINT `track_ibfk_2` FOREIGN KEY (`Student_Num`) REFERENCES `collegestudents` (`UIN`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-INSERT INTO `users` (
-    `UIN`,
-    `First_Name`,
-    `M_Initial`,
-    `Last_Name`,
-    `Username`,
-    `Passwords`,
-    `User_Type`,
-    `Email`,
-    `Discord_Name`
-) VALUES (
-    1, -- Replace with a suitable unique UIN
-    'Admin',
-    NULL,
-    'User',
-    'admin',
-    'admin', -- Replace with the actual hashed password (e.g., bcrypt)
-    'admin', -- Assuming 'Admin' is the user type for administrators
-    'admin@example.com', -- Replace with a valid email address
-    'admin#1234', -- Replace with a valid Discord name and discriminator
-);
-
-INSERT INTO `users` (
-    `UIN`,
-    `First_Name`,
-    `M_Initial`,
-    `Last_Name`,
-    `Username`,
-    `Passwords`,
-    `User_Type`,
-    `Email`,
-    `Discord_Name`
-) VALUES (
-    2, -- Replace with a suitable unique UIN
-    'John',
-    'M',
-    'Doe',
-    'john.doe',
-    'password', -- Replace with the actual hashed password (e.g., bcrypt)
-    'student', -- Assuming 'Student' is the user type for college students
-    'john.doe@example.com', -- Replace with a valid email address
-    'john.doe#5678' -- Replace with a valid Discord name and discriminator
-);
-
-INSERT INTO `collegestudents` (
-    `UIN`,
-    `Gender`,
-    `HispanicLatino`,
-    `Race`,
-    `USCitizen`,
-    `First_Generation`,
-    `DoB`,
-    `GPA`,
-    `Major`,
-    `Minor1`,
-    `Minor2`,
-    `Expected_Graduation`,
-    `School`,
-    `Current_Classification`,
-    `Phone`,
-    `Student_Type`
-) VALUES (
-    2, -- Same UIN as in the users table
-    'Male',
-    0, -- 0 for false, assuming not Hispanic/Latino
-    'Caucasian',
-    1, -- 1 for true, assuming US citizen
-    0, -- 0 for false, assuming not first generation
-    '1990-01-15', -- Replace with the actual date of birth
-    3.5, -- Replace with the actual GPA
-    'Computer Science',
-    'Mathematics',
-    'Psychology',
-    2023, -- Replace with the actual expected graduation year
-    'School of Science and Engineering',
-    'Senior',
-    1234567890, -- Replace with the actual phone number
-    'Undergraduate' -- Replace with the actual student type
-);
